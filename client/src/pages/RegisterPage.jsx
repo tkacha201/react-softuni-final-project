143b742c1ead04c7b9305573250bd42861a19da3
Teodosi Kachakov <teodosi.kachakov@gmail.com>
2023-12-16 23:50:37 +0200

    registration functionality

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/register", {
+        name,
+        email,
+        password,
+      });
+      alert("Success! Please log in.");
+    } catch (error) {
+      alert("Registration failed. Try again later");
+    }
+  };
+
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Register</h1>
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="Full Name (e.g., Jane Doe)"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="example@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="primary">Register</button>
+          <div className="text-center py-2 text-gray-500">
+            Have an account?
+            <Link className="underline text-black" to={"/login"}>
+              Sign in.
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,40 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User.js");
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
 const app = express();
 
+const secretSalt = bcrypt.genSaltSync(10);
+
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+mongoose.connect(process.env.MONGO_URL);
+
 app.get("/test", (req, res) => {
-  res.json("Hi from test");
+  res.json("Hi from test2");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userObj = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, secretSalt),
+    });
+    res.json(userObj);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
 
 app.listen(4000);
