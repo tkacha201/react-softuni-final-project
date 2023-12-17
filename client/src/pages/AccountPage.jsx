4d64ccb664e360b9b73ccaff2426f66f7b0ce946
Teodosi Kachakov <teodosi.kachakov@gmail.com>
2023-12-17 19:18:41 +0200

    my profile page finished

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { UserContext } from "../UserContext";
+import { useContext } from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const AccountPage = () => {
-  return <div>AccountPage2</div>;
+  const [redirect, setRedirect] = useState(null);
+  const { ready, user, setUser } = useContext(UserContext);
+
+  let { subpage } = useParams();
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
+
+  async function logout() {
+    await axios.post("/logout");
+    setRedirect("/");
+    setUser(null);
+  }
+
+  if (!ready) {
+    return <div>Loading...</div>;
+  }
+
+  if (ready && !user && !redirect) {
+    return <Navigate to="/login" />;
+  }
+
+  function linkClasses(type = null) {
+    let classes = "py-2 px-10";
+    if (type === subpage) {
+      classes += " bg-primary text-white rounded-full";
+    }
+    return classes;
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+
+  return (
+    <div>
+      <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+        <Link className={linkClasses("profile")} to={"/account"}>
+          My Profile
+        </Link>
+        <Link className={linkClasses("bookings")} to={"/account/bookings"}>
+          My Bookings
+        </Link>
+        <Link
+          className={linkClasses("experiences")}
+          to={"/account/experiences"}
+        >
+          My Experiences
+        </Link>
+      </nav>
+      {subpage === "profile" && (
+        <div className="text-center max-w-lg mx-auto">
+          Logged in as {user.name} ({user.email})<br />
+          <button onClick={logout} className="primary max-w-sm mt-2">
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AccountPage;
